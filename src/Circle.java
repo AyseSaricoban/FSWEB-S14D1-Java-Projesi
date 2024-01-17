53942723f935fdc4c9834ecc650ef6b3690b62a3
AyseSaricoban <aysesrcbnn@gmail.com>
2024-01-17 05:15:58 +0300

    task1 fin

---
+++ src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        if (radius<0){
+            this.radius=0;
+        }else{
+            this.radius=radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+}
